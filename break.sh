23880d70da6122240bc80bad2553c84ecf304a1b
Randy Fay <randy@randyfay.com>
2021-06-09 16:00:30 -0600

    Use ddev, use port 80

--- break.sh
+++ break.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -eu -o pipefail
 
+ddev --version
 ./docker-desktop-version.sh
 printf "\nArchitecture: $(arch)\n"
+port=80
 
-docker stop nginx >/dev/null 2>&1 || true
+ddev stop >/dev/null 2>&1 || true
 while true; do
-  echo -n .
   ddev start >/dev/null 2>&1 && starttime=$(date)
-  curl -sSL --fail http://dockereofdemo.ddev.site:8080 >/dev/null || ( printf "curl failed at $(date) after ddev start finishing at ${starttime}" && docker inspect --format "{{json .State.Health }}" ddev-dockereofdemo-web && exit 1 )
+  for item in dockereofdemo x y z a.junk b.junk c.junk d.junk; do
+    url="http://${item}.ddev.site:${port}"
+    curl -sSL --fail $url > /dev/null || ( printf "curl failed on $url at $(date) after ddev start finishing at ${starttime}" && ( docker inspect --format "{{json .State.Health }}" ddev-dockereofdemo-web | jq -r ) && ( docker inspect --format "{{json .State.Health }}" ddev-router ) | jq -r && exit 1 )
+  done
+  echo -n .
   ddev stop >/dev/null
 done
