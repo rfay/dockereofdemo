96779f9685fdb7910540657334fe06c3b3dec4d6
Randy Fay <randy@randyfay.com>
2021-06-10 07:39:38 -0600

    Use ddev start -y

--- break.sh
+++ break.sh
@@ -8,7 +8,7 @@ port=80
 
 ddev stop >/dev/null 2>&1 || true
 while true; do
-  ddev start >/dev/null 2>&1 && starttime=$(date)
+  ddev start -y >/dev/null 2>&1 && starttime=$(date)
   for item in dockereofdemo x y z a.junk b.junk c.junk d.junk; do
     url="http://${item}.ddev.site:${port}"
     curl -sSL --fail $url > /dev/null || ( printf "curl failed on $url at $(date) after ddev start finishing at ${starttime}" && ( docker inspect --format "{{json .State.Health }}" ddev-dockereofdemo-web | jq -r ) && ( docker inspect --format "{{json .State.Health }}" ddev-router ) | jq -r && exit 1 )
